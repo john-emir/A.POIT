3a59841464d7594231aa825551adf9e127fc0afb
john-emir <62830033+john-emir@users.noreply.github.com>
2021-12-27 16:35:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log(hi)
+let url = "https://john-emir.github.io/B.POIT/"
+function loadBodyPage(taskId){
+    fetch(url,{
+        method:'GET',
+        mode: 'cors',
+    })
+    .then(function(response){
+        return response.text();
+    }).then(function(data){
+            document.body.insertAdjacentHTML('beforeend', data);
+        
+    }).catch(function(error){
+        alert(error);
+    })
+
+}
+
+loadBodyPage()
+
